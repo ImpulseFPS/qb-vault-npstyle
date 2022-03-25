d4ab90b9b8b3f38db9737dd4335979717fa8e1d9
DarkTV988 <86536680+DarkTV988@users.noreply.github.com>
2022-03-25 18:36:10 +0430

    Add files via upload

---
+++ qb-vault/config/sh_config.lua
@@ -0,0 +1,19 @@
+
+Config = {}
+
+
+Config.StorageDefaultWeight=400000
+Config.StorageAddStep=200000
+Config.StorageAddPrice=3000
+Config.StorageSlots=100
+Config.StorageCreationAmount=5000
+
+Config.passwordSettingCost=500
+
+Config.SaleAvailableForJob=false
+Config.jobName="warehouseman"
+
+Config.location = {
+    [1] = {name="Adams_Apple" , coords= vector3(-43.81, -1236.47, 29.34),heading=270.41,minz=27.36 , maxz=30.87834,length=1,width=3,distance=2.5,debug=false},
+    [2] = {name="Only_ForTreyy" , coords= vector3(-45.52, -1275.12, 29.22),heading=270.41,minz=27.36 , maxz=30.87834,length=1,width=3,distance=2.5,debug=false},
+}
